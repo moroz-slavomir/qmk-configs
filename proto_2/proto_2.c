b2037e045bbf8b5ebac7c143df71e08fc74f2d60
Slavomir Moroz <selepko121@gmail.com>
2020-06-19 19:49:51 +0200

    Add caps lock led to proto_2

--- proto_2/proto_2.c
+++ proto_2/proto_2.c
@@ -6,6 +6,7 @@ void keyboard_pre_init_user(void) {
   // Set our LED pins as output
   setPinOutput(F4);
   setPinOutput(F5);
+  setPinOutput(D4);
 }
 
 
@@ -13,15 +14,23 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
     case 3:
     case 4:
-        writePin(F4, 1);
         writePin(F5, 1);
+        writePin(D4, 1);
         break;
     default: //  for any other layers, or the default layer
-        writePin(F4, 0);
         writePin(F5, 0);
+        writePin(D4, 0);
         break;
     }
   return state;
+}
+
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        writePin(F4, led_state.caps_lock);
+    }
+    return res;
 }
 
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
