0e97be958d7b6690d483f9a647825b03e8701dae
Slavomir Moroz <selepko121@gmail.com>
2020-08-08 00:19:21 +0200

    Updated proto_3

--- proto_3/proto_3.c
+++ proto_3/proto_3.c
@@ -7,12 +7,31 @@ void keyboard_pre_init_user(void) {
   setPinOutput(F4);
 }
 
+void keyboard_post_init_user(void) {
+  rgblight_enable_noeeprom(); // Enables RGB, without saving settings
+  rgblight_sethsv_noeeprom(HSV_CYAN);
+  // rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
+}
+
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if(res) {
         writePin(F4, led_state.caps_lock);
     }
     return res;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case 3:
+    case 4:
+        rgblight_sethsv_noeeprom(HSV_RED);
+        break;
+    default: //  for any other layers, or the default layer
+        rgblight_sethsv_noeeprom(HSV_CYAN);
+        break;
+    }
+  return state;
 }
 
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
